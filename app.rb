cec93f36d5a35a97afbbc1f590e4804003a94c34
katarina-pen <katarinapenava0711@gmail.com>
2026-04-19 15:28:08 +0200

    v.16
    Almost finished with model and helper functions, minor errors left.

--- app.rb
+++ app.rb
@@ -89,9 +89,9 @@ get('/story') do
   
   # db = SQLite3:: Database.new("db/databas.db")
   
-  # db = connect_to_db('db/databas.db')
+  db = connect_to_db('db/databas.db')
   #Fixa eller ta bort helt 
-  @username= db.execute("SELECT username from users WHERE id =?", user_id)
+  # @username= db.execute("SELECT username from users WHERE id =?", user_id)
 
   # # db.results_as_hash = true
   # @usersInventory= db.execute("SELECT 
@@ -112,7 +112,7 @@ get('/story2') do
 end
 
 get('/story3') do
-  user_id = session[:id]
+  # user_id = session[:id]
   # db = SQLite3:: Database.new("db/databas.db")
   # db.results_as_hash = true
   # db = connect_to_db('db/databas.db')
@@ -203,15 +203,18 @@ get('/enemies/:id/fight') do
   # db = SQLite3:: Database.new("db/databas.db")
   # db.results_as_hash = true
   db = connect_to_db('db/databas.db')
-  @enemiesData = db.execute("SELECT * FROM enemies WHERE id = ?",id)
-  @itemsData = db.execute("SELECT * FROM items")
-  @usersInventory= db.execute("SELECT 
-  items.name, items.damage 
-  FROM users_items 
-  INNER JOIN items ON users_items.items_id =items.id 
-  WHERE users_id =?", user_id)
-  @userStats = db.execute("SELECT health FROM users WHERE id =?", user_id)
- 
+  # @enemiesData = db.execute("SELECT * FROM enemies WHERE id = ?",id)
+  # @itemsData = db.execute("SELECT * FROM items")
+  @enemiesData, @itemsData=damage_data(params[:id].to_i)
+
+  # @usersInventory= db.execute("SELECT 
+  # items.name, items.damage 
+  # FROM users_items 
+  # INNER JOIN items ON users_items.items_id =items.id 
+  # WHERE users_id =?", user_id)
+  # @userStats = db.execute("SELECT health FROM users WHERE id =?", user_id)
+  @usersInventory, @userStats=stats_inventory(session[:id].to_i)
+
 
    if @userStats[0]["health"] <= 0
       redirect("/game_over")
@@ -228,16 +231,22 @@ end
 
 post("/enemies/:id/attack") do
   id = params[:id].to_i
-  user_id = session[:id]
-  @itemsDamg = params[:itemsDamage].to_i
-  enemyDamage= params[:enemyDamage].to_i
+  # user_id = session[:id]
+  # @itemsDamg = params[:itemsDamage].to_i
+  # enemyDamage= params[:enemyDamage].to_i
 
   # db = SQLite3::Database.new("db/databas.db")
   db = connect_to_db('db/databas.db')
-  @itemsData = db.execute("SELECT * FROM items")
+  # @itemsData = db.execute("SELECT * FROM items")
   
-  db.execute("UPDATE users SET health=health-? WHERE id =?",[enemyDamage, user_id])
-  db.execute("UPDATE enemies SET health=health-? WHERE id = ?",[@itemsDamg, id])
+  #båda returneras i funktion men jag använder endast 1 här
+  @enemiesData, @itemsData=damage_data(params[:id].to_i)
+
+  # db.execute("UPDATE users SET health=health-? WHERE id =?",[enemyDamage, user_id])
+  update_user_health(params[:enemyDamage].to_i,session[:id] )
+  # db.execute("UPDATE enemies SET health=health-? WHERE id = ?",[@itemsDamg, id])
+  update_enemy_health(params[:itemsDamage].to_i, params[:id].to_i)
+  
   redirect("/enemies/#{id}/fight")
 end 
 
@@ -249,38 +258,56 @@ end
 #SHOP🛒💵💰
 get("/shop") do
   #användarens id, vem som är inloggad
-  user_id = session[:id]
+  # user_id = session[:id]
   # db = SQLite3:: Database.new("db/databas.db")
   # db.results_as_hash = true
-  db = connect_to_db('db/databas.db')
-  @itemsData = db.execute("SELECT * FROM items")
-  @usersData = db.execute("SELECT * FROM users WHERE id = ?", user_id).first
+  # db = connect_to_db('db/databas.db')
+  # @itemsData = db.execute("SELECT * FROM items")
   
-  @usersInventory= db.execute("SELECT 
-  items.name, items.damage 
-  FROM users_items 
-  INNER JOIN items ON users_items.items_id =items.id 
-  WHERE users_id =?", user_id)
-  @userStats = db.execute("SELECT health FROM users WHERE id =?", user_id)
+  #använder mig endast av @itemsData här
+  # @enemiesData, @itemsData=damage_data(params[:id].to_i)
+
+
+  # @usersData = db.execute("SELECT * FROM users WHERE id = ?", user_id)
+  # @usersData, @itemsData = user_item_data(session[:id])
+  @usersData = user_data(session[:id])
+  @itemsData = item_data()
+  p "itemsData är: #{@itemsData}"
+
+
+
+  # @usersInventory= db.execute("SELECT 
+  # items.name, items.damage 
+  # FROM users_items 
+  # INNER JOIN items ON users_items.items_id =items.id 
+  # WHERE users_id =?", user_id)
+  # @userStats = db.execute("SELECT health FROM users WHERE id =?", user_id)
+  @usersInventory, @userStats=stats_inventory(session[:id].to_i)
+
   slim(:shop)
 end
 
 post("/shop/:id/buy") do
   #items id
-  id = params[:id].to_i
-  users_id = session[:id]
-  cost = params[:cost].to_i
+  # id = params[:id].to_i
+  # users_id = session[:id]
+  # cost = params[:cost].to_i
   # db = SQLite3:: Database.new("db/databas.db")
   # db.results_as_hash = true
-  db = connect_to_db('db/databas.db')
+  # db = connect_to_db('db/databas.db')
+  itemExistCheck=unique_items_checker(session[:id], params[:id].to_i)
 
-  itemExistCheck = db.execute("SELECT * FROM users_items WHERE users_id = ? AND items_id = ?",[users_id, id]).first
+  # itemExistCheck = db.execute("SELECT * FROM users_items WHERE users_id = ? AND items_id = ?",[users_id, id]).first
   if itemExistCheck != nil
     # "Du äger redan detta item"
 
   else 
-    db.execute("UPDATE users SET money=money-? WHERE id = ?",[cost, users_id])
-    db.execute("INSERT INTO users_items (users_id, items_id) VALUES (?,?)",[users_id, id] )
+    # db.execute("UPDATE users SET money=money-? WHERE id = ?",[cost, users_id])
+    # db.execute("INSERT INTO users_items (users_id, items_id) VALUES (?,?)",[users_id, id] )
+    # buy_item(cost,user_id,id)
+    buy_item(params[:cost].to_i, users_id = session[:id])
+    get_item(session[:id], params[:id].to_i)
+
   end
 
   redirect("/shop")
--- model.rb
+++ model.rb
@@ -21,14 +21,80 @@ def login_user(username,password)
 end
 
 
-def stats_inventory(user_id)
+
+helpers do
+  def stats_inventory(user_id)
+    db = connect_to_db('db/databas.db')
+    # user_id = session[:id].to_i
+    @usersInventory= db.execute("SELECT 
+    items.name, items.damage 
+    FROM users_items 
+    INNER JOIN items ON users_items.items_id =items.id 
+    WHERE users_id =?", user_id)
+    @userStats = db.execute("SELECT health FROM users WHERE id =?", user_id)
+    return @usersInventory, @userStats
+  end
+end
+
+#Fight 
+helpers do
+  def damage_data(id)
+    db = connect_to_db('db/databas.db')
+    @enemiesData = db.execute("SELECT * FROM enemies WHERE id = ?",id)
+    @itemsData = db.execute("SELECT * FROM items")
+    return @enemiesData, @itemsData
+  end
+end
+
+
+def update_user_health(enemyDamage, user_id)
   db = connect_to_db('db/databas.db')
-  # user_id = session[:id].to_i
-  @usersInventory= db.execute("SELECT 
-  items.name, items.damage 
-  FROM users_items 
-  INNER JOIN items ON users_items.items_id =items.id 
-  WHERE users_id =?", user_id)
-  @userStats = db.execute("SELECT health FROM users WHERE id =?", user_id)
-  return @usersInventory, @userStats
+  db.execute("UPDATE users SET health=health-? WHERE id =?",[enemyDamage, user_id])
+end
+
+def update_enemy_health(itemsDamg,id)
+  db = connect_to_db('db/databas.db')
+  db.execute("UPDATE enemies SET health=health-? WHERE id = ?",[@itemsDamg, id])
+
+end
+
+
+
+#Shop
+helpers do
+  def user_data(user_id)
+    db = connect_to_db('db/databas.db')
+    @usersData = db.execute("SELECT * FROM users WHERE id = ?", user_id).first
+    # @itemsData = db.execute("SELECT * FROM items")
+    return @usersData
+  end
+end
+
+helpers do
+  def item_data()
+    db = connect_to_db('db/databas.db')
+    # @usersData = db.execute("SELECT * FROM users WHERE id = ?", user_id).first
+    @itemsData = db.execute("SELECT * FROM items")
+    return @itemsData
+  end
+end
+
+
+def unique_items_checker(users_id, id)
+  db = connect_to_db('db/databas.db')
+  itemExistCheck = db.execute("SELECT * FROM users_items WHERE users_id = ? AND items_id = ?",[users_id, id]).first
+  return itemExistCheck
+end
+
+def buy_item(cost, users_id) 
+  db = connect_to_db('db/databas.db')
+  db.execute("UPDATE users SET money=money-? WHERE id = ?",[cost, users_id])
+  # db.execute("INSERT INTO users_items (users_id, items_id) VALUES (?,?)",[users_id, id] )
+  # return 
+end
+
+def get_item(users_id,id)
+  db = connect_to_db('db/databas.db')
+  db.execute("INSERT INTO users_items (users_id, items_id) VALUES (?,?)",[users_id, id] )
+
 end
